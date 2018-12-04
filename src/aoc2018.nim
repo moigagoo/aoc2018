945526f546f4f3ebcca21727d9b315b1bd7d2397
Konstantin Molchanov <moigagoo@live.com>
2018-12-04 15:33:13 +0300

    Day 2 task 2.

--- src/aoc2018.nim
+++ src/aoc2018.nim
@@ -6,5 +6,6 @@ when isMainModule:
   dispatchMulti(
     [day1task1, echoResult=true],
     [day1task2, echoResult=true],
-    [day2task1, echoResult=true]
+    [day2task1, echoResult=true],
+    [day2task2, echoResult=true]
   )
--- src/day2.nim
+++ src/day2.nim
@@ -16,3 +16,40 @@ proc day2task1*(path: string): int =
     if 3 in counts: inc triples
 
   doubles * triples
+
+
+func differByTwoChars(s1, s2: string): bool =
+  var diffChars: Natural = 0
+
+  for pair in zip(s1, s2):
+    if pair.a != pair.b: inc diffChars
+    if diffChars > 1: return
+
+  return diffChars == 1
+
+func commonChars(s: seq[string]): string =
+  let m = min(s.mapIt(len(it)))
+
+  for i in 0..<m:
+    let charsAtI = s.mapIt(it[i])
+    if allIt(charsAtI, it == charsAtI[0]): result.add charsAtI[0]
+
+proc day2task2*(path: string): string =
+  ##[
+    Find the lines that differ by exactly one character on the same position
+    and return their common characters as a single string.
+  ]##
+
+  var
+    history: seq[string]
+    correctIds: seq[string]
+
+  for line in lines(path):
+    for visitedLine in history:
+      if differByTwoChars(line, visitedLine):
+        correctIds.add line
+        correctIds.add visitedLine
+
+    history.add line
+
+  result = commonChars(correctIds)

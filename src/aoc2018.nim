53cbb4f1dd604eeab73cf8baa662e03e92c63ada
Konstantin Molchanov <moigagoo@live.com>
2018-12-05 11:33:24 +0400

    Day 3, task 2.

--- src/aoc2018.nim
+++ src/aoc2018.nim
@@ -8,5 +8,6 @@ when isMainModule:
     [day1task2, echoResult=true],
     [day2task1, echoResult=true],
     [day2task2, echoResult=true],
-    [day3task1, echoResult=true]
+    [day3task1, echoResult=true],
+    [day3task2, echoResult=true]
   )
--- src/day3.nim
+++ src/day3.nim
@@ -3,13 +3,15 @@ import tables, strscans, sequtils
 type
   Coords = tuple
     x, y: int
-  Grid = CountTable[Coords]
-
-func initGrid(): Grid = initCountTable[Coords]()
+  Cell = tuple
+    id, value: int
+  Claim = tuple
+    id, x, y, w, h: int
 
 
 proc day3task1*(path: string): int =
-  var grid = initGrid()
+  ## Calculate the number of grid cells occupied by more than one "claim."
+  var grid = initCountTable[Coords]()
 
   for line in lines(path):
     var id, x, y, w, h: int
@@ -21,3 +23,29 @@ proc day3task1*(path: string): int =
         grid.inc (i, j)
 
   len toSeq(grid.values).filterIt(it > 1)
+
+proc day3task2*(path: string): int =
+  var
+    grid = initTable[Coords, Cell]()
+    claims: seq[Claim]
+
+  for line in lines(path):
+    var claim: Claim
+
+    discard scanf(line, "#$i @ $i,$i: $ix$i", claim.id, claim.x, claim.y, claim.w, claim.h)
+
+    claims.add claim
+
+    for i in claim.x..<claim.x+claim.w:
+      for j in claim.y..<claim.y+claim.h:
+        discard grid.hasKeyOrPut((i, j), (claim.id, 0))
+
+        inc grid[(i, j)].value
+
+  for claim in claims:
+    block checkClaim:
+      for i in claim.x..<claim.x+claim.w:
+        for j in claim.y..<claim.y+claim.h:
+          if grid[(i, j)].value != 1: break checkClaim
+
+      return claim.id
